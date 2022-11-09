a8de6272382d4265867db68210c0d0c10d944adc
ygzaydn <ygzaydns@gmail.com>
2022-11-09 10:12:46 +0300

    update

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
-import express from 'express'
+import express from "express";
+import router from "./router";
+import morgan from "morgan";
 
 const app = express();
 
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router, morgan("dev"));
+
 app.get("/", (req, res) => {
-  console.log("we got a get request on /");
-  res.status(200);
-  res.json({ message: "hello" });
+    console.log("we got a get request on /");
+    res.status(200);
+    res.json({ message: "hello" });
 });
 
 export default app;
